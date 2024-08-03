9ac1c81f385fbc69e60a27f8b38c8e7b0c73dab3
sudarshanca <sudarshanreddyca@gmail.com>
2024-08-03 15:34:21 +0530

    lt update

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ function headerSidebar() {
   let menuIcon = document.getElementById("menu-icon");
   let navBlackBg = document.getElementById("nav-black-bg");
   let backgroundSidebar = document.getElementById("nav-items");
+  
   menuIcon.classList.toggle("active");
   navBlackBg.classList.toggle("active");
   backgroundSidebar.classList.toggle("active");
-  menuIcon.classList.remove("active");
 }
+
 
 // header scroll effect
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
-  if (window.scrollY > 100) {
+  if (window.scrollY > 50) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
@@ -100,4 +101,48 @@ document.addEventListener("DOMContentLoaded", () => {
       modalImage.removeAttribute("data-bs-target");
     }
   }
+});
+
+
+    
+// form start
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
